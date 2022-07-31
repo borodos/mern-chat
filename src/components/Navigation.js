4fa1180ea6f98e1e7f35165e64e30dc30af6bc17
borodos <stepllerrus78@gmail.com>
2022-07-31 03:13:10 +0300

    123

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -9,7 +9,7 @@ function Navigation() {
 			<Container>
 				<LinkContainer to="/">
 					<Navbar.Brand>
-						<img src={Logo} style={{ width: 50, height: 50 }} alt="Logo" />
+						<img src={Logo} style={{ width: 50, height: 50 }} alt="Logos" />
 					</Navbar.Brand>
 				</LinkContainer>
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
